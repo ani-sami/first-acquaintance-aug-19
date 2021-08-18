cb6f61d385e4238d410149383d9565198e76c194
ani-sami <84287803+ani-sami@users.noreply.github.com>
2021-08-18 21:41:39 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+var main_cont = document.querySelector('.main-cont')
+var download_btn = document.querySelector('.download-btn')
+var main_cont_btn = document.querySelector('.close-btn')
+var container = document.querySelector('.container')
+var input = document.querySelector('.input')
+var error_cont = document.querySelector('.error-cont')
+var err_btn = document.querySelector('.error-cont a')
+
+var submit = document.querySelector('.btn')
+
+var password = '$putuputu_love@sami&ani%'
+
+function main() {
+  if(input.value == password) {
+    container.classList.toggle('active')
+    main_cont.classList.toggle('active')
+  }
+  else {
+    container.classList.toggle('active')
+    error_cont.classList.toggle('active')
+    input.value = ''
+  }
+}
+
+submit.addEventListener('click', function() {
+  main()
+})
+
+main_cont_btn.addEventListener('click', function() {
+  main()
+})
+
+err_btn.addEventListener('click', function() {
+  main()
+})
